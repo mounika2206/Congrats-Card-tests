54c8b828480ddc3189416c91c0b6cf6ca50eab53
mounika2206 <mounikareddi22@gmail.com>
2022-10-26 06:40:35 +0000

    congrats-card

---
+++ index.js
@@ -0,0 +1,24 @@
+const element = (
+  <div className="bg-container">
+    <h1 className="heading">Congratulations</h1>
+    <div className="card">
+      <div>
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+          className="image"
+        ></img>
+      </div>
+      <h1>Kiran v</h1>
+      <p>Rajiv gandhi University of Knowledge Technologies Basar</p>
+      <div>
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+          className="image"
+        ></img>
+      </div>
+    </div>
+  </div>
+  // Write your code here.
+);
+
+ReactDOM.render(element, document.getElementById("root"));
